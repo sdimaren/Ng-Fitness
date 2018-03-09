c409b2c378e36477f96f94f5b90e2ae319df773a
Santiago Dimaren <sdimaren1@gmail.com>
2018-03-09 17:48:49 -0500

    adding navigation and a sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,8 @@ import {
   MatInputModule,
   MatDatepickerModule,
   MatNativeDateModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+  MatSidenavModule
 } from '@angular/material';
 
 @NgModule({
@@ -18,7 +19,8 @@ import {
     MatInputModule, 
     MatDatepickerModule, 
     MatNativeDateModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSidenavModule
   ],
   exports: [
     MatButtonModule, 
@@ -27,7 +29,8 @@ import {
     MatInputModule, 
     MatDatepickerModule, 
     MatNativeDateModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSidenavModule
   ]
 })
 export class  MaterialModule {}
