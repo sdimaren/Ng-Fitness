79c91a082dd62999cfd72f82477ebceef94809a1
Santiago Dimaren <sdimaren1@gmail.com>
2018-03-18 12:56:37 -0400

    adding a tabs component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,7 +10,8 @@ import {
   MatCheckboxModule,
   MatSidenavModule,
   MatToolbarModule,
-  MatListModule
+  MatListModule,
+  MatTabsModule
 } from '@angular/material';
 
 @NgModule({
@@ -24,7 +25,8 @@ import {
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ],
   exports: [
     MatButtonModule, 
@@ -36,7 +38,8 @@ import {
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ]
 })
 export class  MaterialModule {}
