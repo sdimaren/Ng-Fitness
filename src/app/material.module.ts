7f90f21ce9fabc9a5e79a42ec2206a9435189a12
Santiago Dimaren <sdimaren1@gmail.com>
2018-03-18 14:35:34 -0400

    adding some cards

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,7 +11,8 @@ import {
   MatSidenavModule,
   MatToolbarModule,
   MatListModule,
-  MatTabsModule
+  MatTabsModule,
+  MatCardModule
 } from '@angular/material';
 
 @NgModule({
@@ -26,7 +27,8 @@ import {
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule, 
@@ -39,7 +41,8 @@ import {
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ]
 })
 export class  MaterialModule {}
