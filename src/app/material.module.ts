52fd9b540e783f80bcf2b5854566082d397d23ab
Santiago Dimaren <sdimaren1@gmail.com>
2018-03-19 16:02:47 -0400

    adding a cancel dialog screen

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,7 +14,8 @@ import {
   MatTabsModule,
   MatCardModule,
   MatSelectModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  MatDialogModule
 } from '@angular/material';
 
 @NgModule({
@@ -32,7 +33,8 @@ import {
     MatTabsModule,
     MatCardModule,
     MatSelectModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule
   ],
   exports: [
     MatButtonModule, 
@@ -48,7 +50,8 @@ import {
     MatTabsModule,
     MatCardModule,
     MatSelectModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule
   ]
 })
 export class  MaterialModule {}
