2b9b753c861b97c10d53cb07c83d51d171588595
Santiago Dimaren <sdimaren1@gmail.com>
2018-03-25 14:21:18 -0400

    using and injecting the training service

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,10 +1,15 @@
 import { Exercise } from "./exercise.model";
 
 export class TrainingService {
-  avaliableExercises: Exercise[]= [
+  private avaliableExercises: Exercise[]= [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
+    { id: 'pushups', name: 'Push Ups', duration: 60, calories: 12 }
   ];
+
+  getAvaliableExercises() {
+    return this.avaliableExercises.slice();
+  }
 }
