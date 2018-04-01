9796ac62707bec3af32498881c5fa6a584121433
Santiago Dimaren <sdimaren1@gmail.com>
2018-04-01 18:11:58 -0400

    handling complete and cancel events

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -13,6 +13,7 @@ export class TrainingService {
   ];
 
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
   getAvaliableExercises() {
     return this.avaliableExercises.slice();
@@ -21,6 +22,28 @@ export class TrainingService {
   startExercise(selectedId: string) {
     this.runningExercise = this.avaliableExercises.find(ex => ex.id === selectedId);
     this.exerciseChanged.next({ ...this.runningExercise });
+  }
+
+  completeExercise() {
+    this.exercises.push({ 
+      ...this.runningExercise, 
+      date: new Date(), 
+      state: 'completed' 
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({ 
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100),
+      calories: this.runningExercise.calories * (progress / 100),
+      date: new Date(), 
+      state: 'cancelled' 
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
   }
 
   getRunningExercise() {
